11aa1d742b9c69f3955b96e816e0af363725a917
Danny <live626@gmail.com>
2021-09-15 13:50:16 -0400

    Inicio Proyecto

---
+++ README.md
@@ -0,0 +1,1 @@
+# consultaPesaje-backend
---
+++ src/main/java/com/albo/ConsultaPesajeApplication.java
@@ -0,0 +1,13 @@
+package com.albo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsultaPesajeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsultaPesajeApplication.class, args);
+	}
+
+}
